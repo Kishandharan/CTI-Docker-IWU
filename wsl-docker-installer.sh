2670fb835ea4ad2cac0a458c47817165e889afb1
Kishan <152014111+Kishandharan@users.noreply.github.com>
2024-11-26 08:07:18 +0400

    Update and rename cmds.txt to wsl-docker-installer.sh

--- wsl-docker-installer.sh
+++ wsl-docker-installer.sh
@@ -1,10 +1,10 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt update
-sudo apt install docker-ce
+sudo apt update -y
+sudo apt install docker-ce -y
 sudo systemctl start docker
 sudo systemctl enable docker
-docker --version
